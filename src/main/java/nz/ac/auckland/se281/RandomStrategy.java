ed881ef52d55f3e91b31d07a9ba7953da4a1eacd
skim411 <skim411@aucklanduni.ac.nz>
2023-05-16 03:49:13 +1200

    refactoring random strategy class

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -4,10 +4,11 @@ public class RandomStrategy implements Strategy{
 
     @Override
     public String[] decideJavisInput() {
-        String[] javisInput = new String[2];
-        javisInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
-        
-        return null;
+        String[] randomInput = new String[2];
+        randomInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
+        int min = Integer.valueOf(randomInput[0]) + 1;
+        int max = Integer.valueOf(randomInput[0]) + 5;
+        randomInput[1] = Integer.toString(Utils.getRandomNumber(min, max));        
+        return randomInput;
     }
-    
 }

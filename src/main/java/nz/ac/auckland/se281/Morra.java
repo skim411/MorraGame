3635fc40c4550881f82a8477dd83e6f2a13b1bf2
skim411 <skim411@aucklanduni.ac.nz>
2023-05-15 23:09:42 +1200

    add comment to explain the code

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -23,13 +23,16 @@ public class Morra {
     Boolean validInput = false;
     
     while (!validInput) {
+      // scan player's input
       String input = Utils.scanner.nextLine();
+      // split input to 2 and name first substring as playerFingers and second as playerSum
       String[] finAndSum = input.split(" ", 2);
       playerFingers = Integer.parseInt(finAndSum[0]);
       playerSum = Integer.parseInt(finAndSum[1]);
       validInput = checkInput(input);
       if (!validInput) {
         MessageCli.INVALID_INPUT.printMessage();
+        MessageCli.ASK_INPUT.printMessage();
       } else if (validInput) {
         MessageCli.PRINT_INFO_HAND.printMessage(player, Integer.toString(playerFingers),
         Integer.toString(playerSum));
@@ -38,9 +41,12 @@ public class Morra {
   }
 
   public Boolean checkInput(String input) {
+    // check player's input
     Boolean validInput = false;
+    // if input is not integer, return false
     if (Utils.isInteger(input)) {
       validInput = false;
+      // if fingers is not between 1 and 5 or sum is not between 1 and 10, return false
     } else if ((playerFingers < 1) | (playerFingers > 5) | (playerSum < 1) | (playerSum > 10)) {
       validInput = false;
     } else {

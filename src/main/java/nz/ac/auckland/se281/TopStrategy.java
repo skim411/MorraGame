1be59fd83516e68d51dbce3b409a8ca6e004738c
skim411 <skim411@aucklanduni.ac.nz>
2023-05-16 15:32:20 +1200

    get most ferquent fingers from Human() class and return top Javis Input

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -4,8 +4,12 @@ public class TopStrategy implements Strategy {
 
     @Override
     public String[] decideJavisInput() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'decideJavisInput'");
+        String[] topInput = new String[2];
+        topInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
+        int javisFingers = Integer.valueOf(topInput[0]);
+        Human player = new Human();
+        int mostCommonFingers = player.getMostFrequentFingers();        
+        topInput[1] = Integer.toString(javisFingers + mostCommonFingers);
+        return topInput;
     }
-    
 }

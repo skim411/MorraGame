57e6cd3e112a2a6a39a18caf48858d1d44b1d6c5
skim411 <skim411@aucklanduni.ac.nz>
2023-05-16 15:33:27 +1200

    create getAverageFingers() and getMostFrequentFingers() method in Human class

--- src/main/java/nz/ac/auckland/se281/Human.java
+++ src/main/java/nz/ac/auckland/se281/Human.java
@@ -1,8 +1,12 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Human {
 
   private String[] humanInput;
+  private List<String> historyInput = new ArrayList<>();
 
   public String[] getHumanInput(String playerName) {
     Boolean validInput = false;
@@ -21,9 +25,14 @@ public class Human {
       } else if (validInput) {
         MessageCli.PRINT_INFO_HAND.printMessage(
             playerName, Integer.toString(playerFingers), Integer.toString(playerSum));
+        historyInput.add(input);
       }
     }
     return humanInput;
+  }
+
+  public List<String> getHistoryInput() {
+    return historyInput;
   }
 
   public Boolean checkInput(String input) {
@@ -41,5 +50,41 @@ public class Human {
       validInput = true;
     }
     return validInput;
+  }
+
+  // get average of player's fingers
+  public int getAverageFingers() {
+    int averageFingers = 0;
+    int sum = 0;
+    for (String input : historyInput) {
+      String[] inputSplit = input.split(" ", 2);
+      int playerFingers = Integer.parseInt(inputSplit[0]);
+      sum += playerFingers;
+    }
+    averageFingers = sum / historyInput.size();
+    return averageFingers;
+  }
+
+  // get most frequent fingers of player
+  public int getMostFrequentFingers() {
+    int maxCount = 0;
+    int fingersMaxFreq = 0;
+    for (int i = 0; i < historyInput.size(); i++) {
+      String[] inputSplit = historyInput.get(i).split(" ", 2);
+      int playerFingers = Integer.parseInt(inputSplit[0]);
+      int count = 0;
+      for (int j = 0; j < historyInput.size(); j++) {
+        String[] inputSplit2 = historyInput.get(j).split(" ", 2);
+        int playerFingers2 = Integer.parseInt(inputSplit2[0]);
+        if (playerFingers == playerFingers2) {
+          count++;
+        }
+      }
+      if (count > maxCount) {
+        maxCount = count;
+        fingersMaxFreq = playerFingers;
+      }
+    }
+    return fingersMaxFreq;
   }
 }

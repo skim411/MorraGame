d1187922f2f9de2703498a6a48a6cc6a5ca05eff
skim411 <skim411@aucklanduni.ac.nz>
2023-05-16 13:11:32 +1200

    shift getHumanInput and checkInput method to Human() class

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -6,8 +6,6 @@ public class Morra {
 
   private int numOfRound = 0;
   private String player;
-  private int playerFingers;
-  private int playerSum;
 
   public Morra() {}
 
@@ -16,43 +14,22 @@ public class Morra {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
+  public String getPlayerName() {
+    return player;
+  } 
+
   public void play() {
     numOfRound++;
     MessageCli.START_ROUND.printMessage(Integer.toString(numOfRound));
     MessageCli.ASK_INPUT.printMessage();
-    Boolean validInput = false;
-    
-    while (!validInput) {
-      // scan player's input
-      String input = Utils.scanner.nextLine();
-      // split input to 2 and name first substring as playerFingers and second as playerSum
-      String[] finAndSum = input.split(" ", 2);
-      playerFingers = Integer.parseInt(finAndSum[0]);
-      playerSum = Integer.parseInt(finAndSum[1]);
-      validInput = checkInput(input);
-      if (!validInput) {
-        MessageCli.INVALID_INPUT.printMessage();
-        MessageCli.ASK_INPUT.printMessage();
-      } else if (validInput) {
-        MessageCli.PRINT_INFO_HAND.printMessage(player, Integer.toString(playerFingers),
-        Integer.toString(playerSum));
-      }
-    }
-  }
 
-  public Boolean checkInput(String input) {
-    // check player's input
-    Boolean validInput = false;
-    // if input is not integer, return false
-    if (Utils.isInteger(input)) {
-      validInput = false;
-      // if fingers is not between 1 and 5 or sum is not between 1 and 10, return false
-    } else if ((playerFingers < 1) | (playerFingers > 5) | (playerSum < 1) | (playerSum > 10)) {
-      validInput = false;
-    } else {
-      validInput = true;
-    }
-    return validInput;
+    Human human = new Human();
+    // Javis javis = new Javis(new EasyLevel());
+    String[] humanInput = human.getHumanInput(player);
+    // String[] javisInput = javis.getJavisInput();
+
+
+    
   }
 
   public void showStats() {}

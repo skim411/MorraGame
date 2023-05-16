6923ec1b8fd1724498ca51d6dde2ed0409a53dc5
skim411 <skim411@aucklanduni.ac.nz>
2023-05-16 15:31:36 +1200

    get average from Human() class and return average Javis Input

--- src/main/java/nz/ac/auckland/se281/AverageStrategy.java
+++ src/main/java/nz/ac/auckland/se281/AverageStrategy.java
@@ -4,8 +4,13 @@ public class AverageStrategy implements Strategy{
 
     @Override
     public String[] decideJavisInput() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'decideJavisInput'");
+        String[] averageInput = new String[2];
+        averageInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
+        int javisFingers = Integer.valueOf(averageInput[0]);
+        Human player = new Human();
+        int avg = player.getAverageFingers();        
+        averageInput[1] = Integer.toString(javisFingers + avg);
+        return averageInput;
     }
     
 }

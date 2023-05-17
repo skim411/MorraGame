a621b65a0fe0aef704b37bfc1536bc98b9cae7ae
skim411 <skim411@aucklanduni.ac.nz>
2023-05-17 22:13:06 +1200

    modify the TopStrategy class to get most common player's fingers when number of round is greater than 4

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -2,14 +2,33 @@ package nz.ac.auckland.se281;
 
 public class TopStrategy implements Strategy {
 
-    @Override
-    public String[] decideJavisInput() {
-        String[] topInput = new String[2];
-        topInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
-        int javisFingers = Integer.valueOf(topInput[0]);
-        Human player = new Human();
-        int mostCommonFingers = player.getMostFrequentFingers();        
-        topInput[1] = Integer.toString(javisFingers + mostCommonFingers);
-        return topInput;
+  private int numOfRound;
+
+  public TopStrategy() {
+  }
+
+  public int getMostCommonFingers(Human player) {
+    return player.getMostFrequentFingers();
+  }
+  
+  public int getNumOfRound(Morra game) {
+    numOfRound = game.getRound();
+    return numOfRound;
+  }
+
+  @Override
+  public String[] decideJavisInput() {
+    String[] topInput = new String[2];
+    topInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
+    int javisFingers = Integer.valueOf(topInput[0]);
+    if (numOfRound <= 4) {
+      int min = javisFingers + 1;
+      int max = javisFingers + 5;
+      topInput[1] = Integer.toString(Utils.getRandomNumber(min, max));
+    } else {
+      int mostCommonFingers = getMostCommonFingers(new Human());
+      topInput[1] = Integer.toString(javisFingers + mostCommonFingers);
     }
+    return topInput;
+  }
 }

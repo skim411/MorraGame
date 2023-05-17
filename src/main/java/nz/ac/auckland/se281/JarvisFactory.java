864d04b377973e0044fdd8ffaf95e7716ccd6a67
skim411 <skim411@aucklanduni.ac.nz>
2023-05-18 06:17:17 +1200

    change class name from javisFactory to jarvisFactory

---
+++ src/main/java/nz/ac/auckland/se281/JarvisFactory.java
@@ -0,0 +1,25 @@
+package nz.ac.auckland.se281;
+
+import nz.ac.auckland.se281.Main.Difficulty;
+
+public class JarvisFactory {
+    public DifficultyLevel createDifficulty(Difficulty difficulty) {
+        switch (difficulty) {
+            case EASY:
+                return new EasyLevel(new RandomStrategy());
+        
+            case MEDIUM:
+                return new MediumLevel(new AverageStrategy());
+
+            case HARD:
+                return new HardLevel(new TopStrategy());
+
+            case MASTER:
+                return new MasterType();
+            
+            default:
+                return null;
+        }
+    }
+}
+

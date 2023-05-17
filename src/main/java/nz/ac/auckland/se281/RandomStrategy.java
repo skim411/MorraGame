966a411e76a70b51f1df34fe4efb38d8068400f3
skim411 <skim411@aucklanduni.ac.nz>
2023-05-17 22:14:06 +1200

    modify the RandomStrategy class

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -8,7 +8,7 @@ public class RandomStrategy implements Strategy{
         randomInput[0] = Integer.toString(Utils.getRandomNumber(1, 5));
         int min = Integer.valueOf(randomInput[0]) + 1;
         int max = Integer.valueOf(randomInput[0]) + 5;
-        randomInput[1] = Integer.toString(Utils.getRandomNumber(min, max));        
+        randomInput[1] = Integer.toString(Utils.getRandomNumber(min, max));
         return randomInput;
     }
 }

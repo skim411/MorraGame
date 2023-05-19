f5c7b4fce8dfece54b7eafdb30e067ab1c909fa8
skim411 <skim411@aucklanduni.ac.nz>
2023-05-19 20:20:36 +1200

    create changeStrategy() and setStrategy() method to change the strategy based on the number of round

--- src/main/java/nz/ac/auckland/se281/MasterLevel.java
+++ src/main/java/nz/ac/auckland/se281/MasterLevel.java
@@ -3,13 +3,32 @@ package nz.ac.auckland.se281;
 public class MasterLevel implements DifficultyLevel {
 
   private Strategy master;
+  private int numOfRound;
+  private Human player;
 
-  public MasterLevel(Strategy strategy) {
+  public MasterLevel(int numOfRound, Human player) {
+    this.player = player;
+    this.numOfRound = numOfRound;
+  }
+
+  public void setStrategy(Strategy strategy) {
     this.master = strategy;
+  }
+
+  public void changeStrategy() {
+    // change strategy based on the number of round
+    if (numOfRound <= 3) {
+        setStrategy(new RandomStrategy());
+    } else if (numOfRound > 3 && numOfRound % 2 == 0) {
+        setStrategy(new AverageStrategy(numOfRound, player));
+    } else {
+        setStrategy(new TopStrategy(numOfRound, player));
+    }
   }
 
   @Override
   public String[] getJarvisHand() {
+    changeStrategy();
     return master.decideJarvisHand();
   }
 }

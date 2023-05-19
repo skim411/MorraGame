582a7d67151aa3dfc820b1916dabed9f42ae24fe
skim411 <skim411@aucklanduni.ac.nz>
2023-05-19 20:21:24 +1200

    delete the codes that works as changeStrategy and setStrategy()

--- src/main/java/nz/ac/auckland/se281/JarvisFactory.java
+++ src/main/java/nz/ac/auckland/se281/JarvisFactory.java
@@ -3,26 +3,21 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class JarvisFactory {
+
   public static DifficultyLevel createDifficulty(int numOfRound, Difficulty difficulty, Human player) {
     // create different difficulty level based on the input
     switch (difficulty) {
       case EASY:
-        return new EasyLevel(new RandomStrategy());
+        return new EasyLevel();
 
       case MEDIUM:
-        return new MediumLevel(new AverageStrategy(numOfRound, player));
+        return new MediumLevel(numOfRound, player);
 
       case HARD:
-        return new HardLevel(new TopStrategy(numOfRound, player));
+        return new HardLevel(numOfRound, player);
 
       case MASTER:
-        if (numOfRound <= 3) {
-          return new MasterLevel(new RandomStrategy());
-        } else if (numOfRound > 3 && numOfRound % 2 == 0) {
-          return new MasterLevel(new AverageStrategy(numOfRound, player));
-        } else {
-          return new MasterLevel(new TopStrategy(numOfRound, player));
-        }
+        return new MasterLevel(numOfRound, player);
 
       default:
         return null;

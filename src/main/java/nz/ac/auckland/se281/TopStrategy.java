49766e749e0aac1cbf3ba7a8d8cc08f9b1606909
skim411 <skim411@aucklanduni.ac.nz>
2023-05-21 03:44:27 +1200

    rewrite the command

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -17,14 +17,14 @@ public class TopStrategy implements Strategy {
     String[] topJavisHand = new String[2];
     topJavisHand[0] = Integer.toString(Utils.getRandomNumber(1, 5));
     int jarvisFingers = Integer.valueOf(topJavisHand[0]);
-    // if number of round is less than 5, jarvis will randomly choose a number
+    // if number of round is less than 4, jarvis will randomly choose a number
     if (numOfRound < 4) {
       int min = jarvisFingers + 1;
       int max = jarvisFingers + 5;
       topJavisHand[1] = Integer.toString(Utils.getRandomNumber(min, max));
     } else {
-      // if number of round is greater or equal to 5, jarvis will get the most common fingers
-      // that player has played
+      // if number of round is greater or equal to 4, jarvis will get the most common fingers
+      // that player played
       int mostCommonFingers = getMostFrequentFingers();
       topJavisHand[1] = Integer.toString(jarvisFingers + mostCommonFingers);
     }

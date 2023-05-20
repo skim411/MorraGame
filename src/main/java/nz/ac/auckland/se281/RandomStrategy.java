c3c908379dd0d1e81b1b9e21516d92c1715551d3
skim411 <skim411@aucklanduni.ac.nz>
2023-05-21 03:43:48 +1200

    rewrite the command

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -4,7 +4,7 @@ public class RandomStrategy implements Strategy {
 
   @Override
   public String[] decideJarvisHand() {
-    // select fingers of Javis and sum of Javis's and Human's hand randomly
+    // select fingers and sum of Jarvis' hand randomly
     String[] randomJavisHand = new String[2];
     randomJavisHand[0] = Integer.toString(Utils.getRandomNumber(1, 5));
     int min = Integer.valueOf(randomJavisHand[0]) + 1;

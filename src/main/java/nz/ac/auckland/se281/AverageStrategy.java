9ff462b738033691b2a37128b08ed43f6ae1c9e0
skim411 <skim411@aucklanduni.ac.nz>
2023-05-21 03:45:41 +1200

    rewrite the command

--- src/main/java/nz/ac/auckland/se281/AverageStrategy.java
+++ src/main/java/nz/ac/auckland/se281/AverageStrategy.java
@@ -18,14 +18,14 @@ public class AverageStrategy implements Strategy {
     averageJavisHand[0] = Integer.toString(Utils.getRandomNumber(1, 5));
     int jarvisFingers = Integer.valueOf(averageJavisHand[0]);
 
-    // if number of round is less than 5, jarvis will randomly choose sum
+    // if number of round is less than 4, jarvis will randomly choose sum
     if (numOfRound < 4) {
       int min = jarvisFingers + 1;
       int max = jarvisFingers + 5;
       averageJavisHand[1] = Integer.toString(Utils.getRandomNumber(min, max));
     } else {
-      // if number of round is greater or equal to 5, jarvis will get the average fingers that
-      // player has played
+      // if number of round is greater or equal to 4, jarvis will get the average fingers that
+      // player played
       int avg = getAverageFingers();
       averageJavisHand[1] = Integer.toString(jarvisFingers + avg);
     }
